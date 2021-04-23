e7c5761582970f4bf97835d15e51d22e4f91dca8
Faizan-Ul-Haq-A <contactfaizanulhaq@gmail.com>
2021-04-23 12:53:16 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -294,7 +294,13 @@ const fetchData = () => {
       });
     });
 };
-
+var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  if (!isChrome){
+      $('#iframeAudio').remove()
+  }
+  else {
+      $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+  }
 // Run fetch and animation in sequence
 const resolveFetch = () => {
   return new Promise((resolve, reject) => {
